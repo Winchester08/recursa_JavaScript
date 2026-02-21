d3cf80356143e7e4870533ea90860175a9c9ff68
Winchester08 <shnaida408@gmail.com>
2026-02-20 22:37:02 -0600

    examples of JS data types

--- repaso/tipos_datos.js
+++ repaso/tipos_datos.js
@@ -5,4 +5,33 @@
 // tipo float
 // tipo objeto
 
-let a = "Cadena en JavaScript";
+let a = "JavaScript es un lenguaje tipado";//tipo cadena
+
+console.log(a.toLocaleUpperCase()); 
+console.log(a.toLowerCase());
+//console.log(a.length);
+let a1 = a.slice(0, 32);
+console.log(a1.replace(" ", "_"));
+
+let x = "colibri ";
+console.log(x.length);
+let p = x.toLocaleUpperCase();
+
+console.log(a.replace("lenguaje", p));
+//console.log(a.includes("perro"));
+
+console.log(a.slice(11, 25));
+
+let edad = 34;// tipo entero
+
+let impuesto = 15.6; // tipo flotante
+
+console.log(typeof(edad));
+console.log(typeof(impuesto));
+
+let decision = false //tipo de dato booleano
+console.log(typeof(decision)); 
+
+let arreglo = ["perro", "caballo", "borrego", "gallo"]; //tipo de dato objeto
+console.log(typeof(arreglo));
+
